9dba853921111bc4ba8780247a5f0046333dc58c
Rasmus Andersson <rasmus@notion.se>
2023-03-05 12:24:32 -0800

    ditch host_os global in favor for target_default()

--- src/main.c
+++ src/main.c
@@ -22,16 +22,15 @@ static char _coroot[PATH_MAX];
 const char* coroot = _coroot;
 u32 comaxproc = 1;
 
-static CoLLVMOS host_os;
-
 extern int main_build(int argc, char*const* argv); // build.c
 
-static linkerfn_t nullable ld_impl(CoLLVMOS os);
+static linkerfn_t nullable ld_impl(sys_t);
 static const char* ld_impl_name(linkerfn_t nullable f);
 
 
 static void usage(FILE* f) {
-  linkerfn_t ldf = ld_impl(host_os);
+  // ld usage text
+  linkerfn_t ldf = ld_impl(target_default()->sys);
   char host_ld[128];
   if (ldf) {
     snprintf(host_ld, sizeof(host_ld),
@@ -45,9 +44,11 @@ static void usage(FILE* f) {
     "Usage: %s <command> [args ...]\n"
     "Commands:\n"
     "  build [args ...]     Compis compiler\n"
+    "  help, --help         Print help on stdout and exit\n"
+    "  version, --version   Print version on stdout and exit\n"
     "Extra commands:\n"
     "  targets              List supported targets\n"
-    "  cc [args ...]        Clang (C & C++ compiler)\n"
+    "  cc [args ...]        C compiler (Clang)\n"
     "  as [args ...]        LLVM assembler (same as cc -cc1as)\n"
     "  ar [args ...]        Object archiver\n"
     "%s" // ld for host, if any
@@ -55,9 +56,12 @@ static void usage(FILE* f) {
     "  ld-elf [args ...]    Linker for ELF\n"
     "  ld-macho [args ...]  Linker for Mach-O\n"
     "  ld-wasm [args ...]   Linker for WebAssembly\n"
+    "For help with a specific command:\n"
+    "  %s <command> --help\n"
     "",
     coprogname,
-    host_ld);
+    host_ld,
+    coprogname);
 }
 
 
@@ -78,31 +82,11 @@ void print_co_version() {
 
 static int ar_main(int argc, char*const* argv) {
   // TODO: accept --target triple (where we really only parse the os)
-
-  // const char* osname = (strlen(argv[0]) > 2) ? &argv[0][3] : "";
-  // CoLLVMOS os = OSDarwin; // TODO: ifdef ... select host
-  // if (strlen(osname) > 0) {
-  //   os = OSTypeParse(osname);
-  //   if (os == OSUnknown) {
-  //     fprintf(stderr,
-  //       "%s: unknown archiver %s;"
-  //       " expected one of -ar, -ar.darwin, -ar.freebsd, -ar.ios, -ar.linux,"
-  //       " -ar.macosx, -ar.openbsd, -ar.win32\n",
-  //       parentprog, argv[0]);
-  //     return 1;
-  //   }
-  // }
-  // if (argc < 3) {
-  //   log("usage: %s %s <archive> <file> ...", parentprog, argv[0]);
-  //   return 1;
-  // }
-
   const char* outfile = argv[1];
   char*const* filesv = &argv[2];
   u32 filesc = argc-2;
   char* errmsg = "?";
   CoLLVMArchiveKind arkind = llvm_sys_archive_kind(target_default()->sys);
-
   err_t err = llvm_write_archive(arkind, outfile, filesv, filesc, &errmsg);
   if (!err)
     return 0;
@@ -121,63 +105,26 @@ static const char* ld_impl_name(linkerfn_t nullable f) {
 }
 
 
-static linkerfn_t nullable ld_impl(CoLLVMOS os) {
-  switch (os) {
-    case CoLLVMOS_Darwin:
-    case CoLLVMOS_MacOSX:
-    case CoLLVMOS_IOS:
-    case CoLLVMOS_TvOS:
-    case CoLLVMOS_WatchOS:
+static linkerfn_t nullable ld_impl(sys_t sys) {
+  switch ((enum target_sys)sys) {
+    case SYS_macos:
       return LLDLinkMachO;
-
-    case CoLLVMOS_Win32:
-      return LLDLinkCOFF;
-
-    case CoLLVMOS_WASI:
-    case CoLLVMOS_Emscripten:
-      return LLDLinkWasm;
-
-    // assume the rest uses ELF (this is probably not correct)
-    case CoLLVMOS_Ananas:
-    case CoLLVMOS_CloudABI:
-    case CoLLVMOS_DragonFly:
-    case CoLLVMOS_FreeBSD:
-    case CoLLVMOS_Fuchsia:
-    case CoLLVMOS_KFreeBSD:
-    case CoLLVMOS_Linux:
-    case CoLLVMOS_Lv2:
-    case CoLLVMOS_NetBSD:
-    case CoLLVMOS_OpenBSD:
-    case CoLLVMOS_Solaris:
-    case CoLLVMOS_Haiku:
-    case CoLLVMOS_Minix:
-    case CoLLVMOS_RTEMS:
-    case CoLLVMOS_NaCl:
-    case CoLLVMOS_AIX:
-    case CoLLVMOS_CUDA:
-    case CoLLVMOS_NVCL:
-    case CoLLVMOS_AMDHSA:
-    case CoLLVMOS_PS4:
-    case CoLLVMOS_ELFIAMCU:
-    case CoLLVMOS_Mesa3D:
-    case CoLLVMOS_Contiki:
-    case CoLLVMOS_AMDPAL:
-    case CoLLVMOS_HermitCore:
-    case CoLLVMOS_Hurd:
+    case SYS_linux:
       return LLDLinkELF;
-
-    default:
+    // case SYS_windows:
+    //   return LLDLinkCOFF;
+    // case SYS_wasi:
+    // case SYS_wasm:
+    //   return LLDLinkWasm;
+    case SYS_none:
+    case SYS_COUNT:
       return NULL;
   }
 }
 
 
 static int ld_main(int argc, char* argv[]) {
-  linkerfn_t impl = ld_impl(host_os);
-  if (!impl) {
-    log("%s: unsupported host OS %s", coprogname, CoLLVMOS_name(host_os));
-    return 1;
-  }
+  linkerfn_t impl = safechecknotnull( ld_impl(target_default()->sys) );
   return !impl(argc, argv, true);
 }
 
@@ -209,11 +156,14 @@ int main(int argc, char* argv[]) {
   const char* cmd = is_multicall ? coprogname : argv[1] ? argv[1] : "";
   usize cmdlen = strlen(cmd);
 
+  if (cmdlen == 0) {
+    log("%s: missing command; try `%s help`", coprogname, coprogname);
+    return 1;
+  }
+
   err_t err = llvm_init();
   if (err)
     errx(1, "llvm_init: %s", err_str(err));
-
-  host_os = LLVMGetHostOS();
 
   #define ISCMD(s) (cmdlen == strlen(s) && memcmp(cmd, (s), cmdlen) == 0)
 
@@ -249,17 +199,12 @@ int main(int argc, char* argv[]) {
   if ISCMD("ld-coff")  return LLDLinkCOFF(argc, argv, true) ? 0 : 1;
   if ISCMD("ld-wasm")  return LLDLinkWasm(argc, argv, true) ? 0 : 1;
 
-  if (cmdlen == 0) {
-    log("%s: missing command (try %s -h)", coprogname, coprogname);
-    return 1;
-  }
-
-  if (strstr(cmd, "-h") || strstr(cmd, "help")) {
+  if (ISCMD("help") || ISCMD("--help") || ISCMD("-h")) {
     usage(stdout);
     return 0;
   }
 
-  if (strstr(cmd, "--version")) {
+  if (ISCMD("version") || ISCMD("--version")) {
     print_co_version();
     return 0;
   }
