5d5f8ca87566e0f9874584e525c4e03e79e58c58
Rasmus Andersson <rasmus@notion.se>
2023-03-14 14:53:35 -0700

    adds basic WASI tests

---
+++ test/tests/c-wasi.sh
@@ -0,0 +1,8 @@
+
+co cc --target=wasm32-wasi hello.c -o hello.wasm
+
+if command -v wasmtime >/dev/null; then
+  wasmtime hello.wasm
+else
+  _warn "not running build product (wasmtime not found in PATH)"
+fi
---
+++ test/tests/cc-wasi.sh
@@ -0,0 +1,8 @@
+
+co c++ --target=wasm32-wasi hello.cc -o hello.wasm
+
+if command -v wasmtime >/dev/null; then
+  wasmtime hello.wasm
+else
+  _warn "not running build product (wasmtime not found in PATH)"
+fi
