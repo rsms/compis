ccc0e8d04ff199df51b4710c1e2ee10511124a4d
Rasmus Andersson <rasmus@notion.se>
2023-03-22 12:39:11 -0700

    improvements to custom-sysroot, testing -isysroot and libc++ in addition to --sysroot and libc

--- test/tests/custom-sysroot.sh
+++ test/tests/custom-sysroot.sh
@@ -1,19 +1,25 @@
-# build a C program with a custom --sysroot
+# Build a C program with a custom --sysroot and -isysroot.
+# Use wasi sysroot since it's small.
+CFLAGS="--target=wasm32-wasi"
 
-# use wasi sysroot since it's small
-# Also, custom macos sysroot requires specifying -platform_version to linker
+# query for effective sysroot
 SYSROOT=$(
-  cc --target=wasm32-wasi -v -c hello.c 2>/dev/null |
+  c++ $CFLAGS -c hello.cc -### 2>/dev/null |
   grep -E '^sysroot=' |
   sed -E -e 's/^sysroot=(.+)/\1/')
 
 echo "using custom sysroot: $SYSROOT"
+[[ "$SYSROOT" == *"/" ]] && _err "SYSROOT ends with slash"
+
+# build actual sysroot if needed
+[ -d "$SYSROOT" ] || c++ $CFLAGS -c hello.cc
 
 # try both "--sysroot=" and "--sysroot" command-line arg flavors
 # since these take different code paths in cc.c
-cc --target=wasm32-wasi --sysroot="$SYSROOT" hello.c -o hello.wasm
-cc --target=wasm32-wasi --sysroot "$SYSROOT" hello.c -o hello.wasm
+c++ $CFLAGS --sysroot="$SYSROOT" hello.cc -o hello.wasm
+c++ $CFLAGS --sysroot "$SYSROOT" hello.cc -o hello.wasm
+wasi-run hello.wasm
 
-if command -v wasmtime >/dev/null; then
-  wasmtime hello.wasm
-fi
+# test -isysroot
+c++ $CFLAGS -isysroot "$SYSROOT" hello.cc -o hello.wasm
+wasi-run hello.wasm
