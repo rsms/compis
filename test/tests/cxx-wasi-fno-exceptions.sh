8188810f1ae2cc6c0391f00ca128681d953d1a4d
Rasmus Andersson <rasmus@notion.se>
2023-03-27 19:02:02 -0700

    remove parallelism from test cxx-wasi-fno-exceptions.sh (wait without pid doesn't propagate errors and also this particular test causes sync on file lock anyways so doesn't gain from running in parallel)

--- test/tests/cxx-wasi-fno-exceptions.sh
+++ test/tests/cxx-wasi-fno-exceptions.sh
@@ -1,7 +1,7 @@
 # wasi doesn't support exceptions, so -fno-exceptions should have no effect
-c++ --target=wasm32-wasi hello.cc -o a.wasm &
-c++ --target=wasm32-wasi hello.cc -o b.wasm -fno-exceptions
-wait
 
-[ $(_sha256 a.wasm) = $(_sha256 b.wasm) ] ||
+c++ --target=wasm32-wasi hello.cc -o a.wasm
+c++ --target=wasm32-wasi hello.cc -o b.wasm -fno-exceptions
+
+[ "$(_sha256 a.wasm)" = "$(_sha256 b.wasm)" ] ||
   _err "-fno-exceptions caused different build results"
