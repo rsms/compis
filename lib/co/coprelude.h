68b999428ed98729abc6898f4b0ac7ddb1c54147
Rasmus Andersson <rasmus@notion.se>
2023-06-24 10:39:03 -0700

    disable debug printing in __co_mem_dup and __co_mem_free

--- lib/co/coprelude.h
+++ lib/co/coprelude.h
@@ -44,12 +44,12 @@ __attribute__((__noreturn__)) void abort(void);
 
 inline static void* __co_mem_dup(const void* src, __co_uint size) {
   void* ptr = __builtin_memcpy(__builtin_malloc(size), src, size);
-  __builtin_printf("__co_mem_dup(%p, %lu) => %p\n", src, size, ptr);
+  // __builtin_printf("__co_mem_dup(%p, %lu) => %p\n", src, size, ptr);
   return ptr;
 }
 
 inline static void __co_mem_free(void* ptr, __co_uint size) {
-  __builtin_printf("__co_mem_free(%p, %lu)\n", ptr, size);
+  // __builtin_printf("__co_mem_free(%p, %lu)\n", ptr, size);
   __builtin_free(ptr);
 }
 
