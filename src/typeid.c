17c8c8ed83581ca43249f490f8d5aaf3f9c10ed0
Rasmus Andersson <rasmus@notion.se>
2023-06-24 15:54:19 -0700

    fixes typeid computation that would write NUL bytes in some cases

--- src/typeid.c
+++ src/typeid.c
@@ -20,8 +20,20 @@ static void append(buf_t* buf, type_t* t);
 // }
 
 
+static void write_u32(buf_t* buf, u32 v) {
+  buf_print_u32(buf, v, 16);
+  buf_push(buf, ';');
+}
+
+
+static void write_u64(buf_t* buf, u64 v) {
+  buf_print_u64(buf, v, 16);
+  buf_push(buf, ';');
+}
+
+
 static void funtype(buf_t* buf, funtype_t* t) {
-  buf_print_leb128_u32(buf, t->params.len);
+  write_u32(buf, t->params.len);
   for (u32 i = 0; i < t->params.len; i++) {
     local_t* param = t->params.v[i];
     assert(param->kind == EXPR_PARAM);
@@ -32,7 +44,7 @@ static void funtype(buf_t* buf, funtype_t* t) {
 
 
 static void structtype(buf_t* buf, structtype_t* t) {
-  buf_print_leb128_u32(buf, t->fields.len);
+  write_u32(buf, t->fields.len);
   for (u32 i = 0; i < t->fields.len; i++) {
     local_t* field = t->fields.v[i];
     assert(field->kind == EXPR_FIELD);
@@ -42,14 +54,14 @@ static void structtype(buf_t* buf, structtype_t* t) {
 
 
 static void arraytype(buf_t* buf, arraytype_t* t) {
-  buf_print_leb128_u32(buf, t->len);
+  write_u64(buf, t->len);
   append(buf, t->elem);
 }
 
 
 static void aliastype(buf_t* buf, aliastype_t* t) {
   usize namelen = strlen(t->name);
-  buf_print_leb128_u32(buf, (u32)namelen);
+  write_u32(buf, (u32)namelen);
   buf_append(buf, t->name, namelen);
   // TODO: fully-qualified name
 }
@@ -70,24 +82,51 @@ static void append(buf_t* buf, type_t* t) {
   usize bufstart = buf->len;
   buf_push(buf, TYPEID_PREFIX(t->kind));
 
+  dlog("mk typeid %s '%c'", nodekind_name(t->kind), TYPEID_PREFIX(t->kind));
+
   switch (t->kind) {
     case TYPE_ARRAY:    arraytype(buf, (arraytype_t*)t); break;
     case TYPE_FUN:      funtype(buf, (funtype_t*)t); break;
-    case TYPE_PTR:      append(buf, ((ptrtype_t*)t)->elem); break;
-    case TYPE_REF:
-    case TYPE_MUTREF:   append(buf, ((reftype_t*)t)->elem); break;
-    case TYPE_SLICE:
-    case TYPE_MUTSLICE: append(buf, ((slicetype_t*)t)->elem); break;
     case TYPE_OPTIONAL: append(buf, ((opttype_t*)t)->elem); break;
     case TYPE_STRUCT:   structtype(buf, (structtype_t*)t); break;
     case TYPE_ALIAS:    aliastype(buf, (aliastype_t*)t); break;
-    default:            assertf(0, "unexpected %s", nodekind_name(t->kind));
+
+    case TYPE_PTR:
+    case TYPE_REF:
+    case TYPE_MUTREF:
+    case TYPE_SLICE:
+    case TYPE_MUTSLICE:
+      append(buf, ((ptrtype_t*)t)->elem);break;
+
+    default:
+      assertf(0, "unexpected %s", nodekind_name(t->kind));
   }
 
-  // update t
   // note: may have been set in duplicate code branch
-  if (!t->tid)
-    t->tid = sym_intern(&buf->bytes[bufstart], buf->len - bufstart);
+  if (t->tid)
+    return;
+
+  #if DEBUG
+    char tmp[128];
+    {
+      abuf_t s = abuf_make(tmp, sizeof(tmp));
+      abuf_repr(&s, &buf->bytes[bufstart], buf->len - bufstart);
+      abuf_terminate(&s);
+      dlog("%s buf = \"%s\"", nodekind_name(t->kind), tmp);
+    }
+  #endif
+
+  // update t
+  t->tid = sym_intern(&buf->bytes[bufstart], buf->len - bufstart);
+
+  #if DEBUG
+    {
+      abuf_t s = abuf_make(tmp, sizeof(tmp));
+      abuf_repr(&s, t->tid, strlen(t->tid));
+      abuf_terminate(&s);
+      dlog("_typeid(%s) => \"%s\"", nodekind_name(t->kind), tmp);
+    }
+  #endif
 }
 
 
@@ -105,14 +144,6 @@ sym_t _typeid(type_t* t) {
   buf_dispose(&buf);
 
   assertnotnull(t->tid); // else append failed
-
-  // #if DEBUG
-  //   char tmp[128];
-  //   abuf_t s = abuf_make(tmp, sizeof(tmp));
-  //   abuf_repr(&s, t->tid, strlen(t->tid));
-  //   abuf_terminate(&s);
-  //   dlog("_typeid(%s) => \"%s\"", nodekind_name(t->kind), tmp);
-  // #endif
 
   return t->tid;
 }
